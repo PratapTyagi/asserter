046ab50d5f27aaf8d0f2f63f59d6ba6625a3d039
Pratap Tyagi <tyagipratap111@gmail.com>
2021-05-13 18:30:50 +0530

    Completed 'Sidebar' styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Feed from "./components/feed/Feed";
 import SignInSignUp from "./components/signin-signup/SignInSignUp";
 
+import "./App.css";
 const App = () => {
   return (
     <div className="app">
       {/* <h1>Asserter</h1> */}
       <Sidebar />
-      {/* <Feed />
-      <SignInSignUp /> */}
+      <Feed />
+      {/* <SignInSignUp /> */}
     </div>
   );
 };
