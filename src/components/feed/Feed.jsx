847ea65832eb2d9f9a1c8773e546ecb22cbd57e0
Pratap Tyagi <tyagipratap111@gmail.com>
2021-05-14 13:52:39 +0530

    Revert "Now data is comming from firestore"
    This reverts commit 8f09796d226bf759d8cc43c57533531b4842e2d6.

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,18 +1,8 @@
-import { useState, useEffect } from "react";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
-import db from "../../firebase";
 
 import "./Feed.css";
 const Feed = () => {
-  const [posts, setposts] = useState([]);
-
-  useEffect(() => {
-    db.collection("posts").onSnapshot((snapshot) =>
-      setposts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
-    );
-  }, [posts]);
-
   return (
     <div className="feed">
       <div className="feed_header">
@@ -23,17 +13,11 @@ const Feed = () => {
       <TweetBox />
 
       {/* Posts */}
-      {posts.map(({ id, data }) => (
-        <Post
-          key={id}
-          avatar={data.avatar}
-          displayName={data.displayName}
-          username={data.username}
-          verified={data.verified}
-          text={data.text}
-          image={data.image}
-        />
-      ))}
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </div>
   );
 };
